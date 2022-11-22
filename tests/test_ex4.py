c7a0e9b31753c83470bc38b89320d259532df9e0
Helton Teixeira de Souza <helton.souza@ongame.corp.com.br>
2022-11-22 10:44:49 -0300

    Introducing Fixtures and Fixture Factory

--- tests/test_ex4.py
+++ tests/test_ex4.py
@@ -1,24 +1,36 @@
 import pytest
 
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 
-@pytest.fixture()
-def user_1(db):
-    return User.objects.create_user('test-user')
+# @pytest.fixture()
+# def user_1(db):
+#     return User.objects.create_user('test-user')
+#
+#
+# @pytest.mark.django_db
+# def test_set_check_password(user_1):
+#     user_1.set_password('new-password')
+#     assert user_1.check_password('new-password') is True
 
 
-@pytest.mark.django_db
-def test_set_check_password(user_1):
-    user_1.set_password('new-password')
-    assert user_1.check_password('new-password') is True
+# def user_1(db):
+#     user = User.objects.create_user('test-user')
+#     return user
 
-@pytest.fixture()
-def user_1(db):
-    user = User.objects.create_user('test-user')
-    return user
+# def test_set_check_password(user_1):
+#     assert user_1.username == 'test-user'
 
 
-@pytest.mark.django_db
-def test_set_check_password(user_1):
-    assert user_1.username == 'test-user'
+# def test_set_check_password2(user_1):
+#     print('check-user2')
+#     assert user_1.username == 'test-user'
+
+def test_new_user(new_user1):
+    print(new_user1.first_name)
+    assert new_user1.first_name == 'MyName'
+
+
+def test_new_user2(new_user2):
+    print(new_user2.is_staff)
+    assert new_user2.is_staff
